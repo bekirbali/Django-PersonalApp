f8c4b99f8cd637b45c70a7cbeea08c56d4c8592a
bekirbali <bfbali43@gmail.com>
2023-06-13 18:50:42 +0300

    admin site

--- personal_app/admin.py
+++ personal_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import (
+    Department,
+    Personnel
+)
+
+admin.site.register(Department)
+admin.site.register(Personnel)
