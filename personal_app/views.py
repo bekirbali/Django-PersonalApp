e06ae7a2a492afee2eec092b9b211f9a16f2da50
bekirbali <bfbali43@gmail.com>
2023-06-13 21:41:24 +0300

    version 1

--- personal_app/views.py
+++ personal_app/views.py
@@ -2,5 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    pass
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from .serializers import (
+    DepartmentSerializer,
+    PersonnelSerializer
+)
+
+from .models import (
+    Department,
+    Personnel
+)
+
+
+class DepartmentListCreateView(ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class DepartmentRUDView(RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class PersonnelListCreateView(ListCreateAPIView):
+    queryset = Personnel.objects.all()
+    serializer_class = PersonnelSerializer
+
+class PersonnelRUDView(RetrieveUpdateDestroyAPIView):
+    queryset = Personnel.objects.all()
+    serializer_class = PersonnelSerializer
+
